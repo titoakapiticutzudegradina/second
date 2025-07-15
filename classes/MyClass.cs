80bf1c2abf2415831d591485aa0fb32ac63b9fd4
HiMyNameIsGarch <gabrielhanu700@yahoo.com>
2025-07-15 14:30:34 +0300

    new commit about the code

--- classes/MyClass.cs
+++ classes/MyClass.cs
@@ -1,3 +1,10 @@
-class MyClass {
+class Car
+{
+  string color = "myfunnycolorthatisnotred";
 
-};
+  static void Main(string[] args)
+  {
+    Car myObj = new Car();
+    Console.WriteLine(myObj.color);
+  }
+}
